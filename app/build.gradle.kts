ea483b50d8598cb4ed84d4e21ca8142e9c811025
Sameer Ali <140128371+sameer43786@users.noreply.github.com>
2026-08-20 11:52:17 +0200

    Bump Sameer App Awake to v1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.sameerali.appawake"
         minSdk = 29
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.6.0"
+        versionCode = 12
+        versionName = "1.7.0"
     }
 
     buildTypes {
