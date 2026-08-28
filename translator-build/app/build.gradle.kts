a1ffd1f1d1a596652e3e81a4c8694bb64e6410d7
Sameer Ali <140128371+sameer43786@users.noreply.github.com>
2026-08-28 11:50:43 +0200

    Migrate translator build to AGP 9 built-in Kotlin

--- translator-build/app/build.gradle.kts
+++ translator-build/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -27,10 +26,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 
     packaging {
