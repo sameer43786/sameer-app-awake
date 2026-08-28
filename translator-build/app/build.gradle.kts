ae3159c38101b6e80ad41db4f800ca33aa3ed0c3
Sameer Ali <140128371+sameer43786@users.noreply.github.com>
2026-08-28 11:48:41 +0200

    Use stable Android API 36 for translator CI build

--- translator-build/app/build.gradle.kts
+++ translator-build/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.sameer.livetranslator"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.sameer.livetranslator"
